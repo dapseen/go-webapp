a97709a67a4a0ee03824d5df0fd4f26bb690a744
adedapo ajuwon <dapseen@gmail.com>
2019-02-10 10:59:26 +0100

    single view for article

--- handlers.article.go
+++ handlers.article.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,4 +25,29 @@ func showIndexPage(c *gin.Context) {
 		},
 	)
 
+}
+
+func getArticle(c *gin.Context) {
+	//check if the article ID is valid
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		//check if the articke exists
+		if article, err := getArticleByID(articleID); err == nil {
+			//call the HMTL method of the context to render a template
+			c.HTML(
+				http.StatusOK,
+
+				"article.html",
+
+				gin.H{
+					"title":   article.Title,
+					"payload": article,
+				},
+			)
+		} else {
+			c.AbortWithError(http.StatusNotFound, err)
+		}
+	} else {
+		//if an invalid article ID is specified in the URL
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
